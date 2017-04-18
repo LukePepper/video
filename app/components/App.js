ebdeca3e67d9d8548d1ff5afbdb8cb33cd6cfe66
Luke Pepper <luke.pepper@wearefetch.com>
2017-04-18 14:22:53 +0100

    first commit

---
+++ app/components/App.js
@@ -0,0 +1,10 @@
+var React = require('react');
+
+var testComponent=React.createClass({
+    render: function () {
+        return <h1>Hello World!</h1>;
+    }
+});
+
+module.exports=testComponent;
+
