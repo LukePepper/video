12ab3bdee094b0faece08d0fd8cf85019f91c64a
Luke Pepper <luke.pepper@wearefetch.com>
2017-04-20 16:36:05 +0100

    pulling data from internal JSON and rendering correctly

--- app/components/Videos.js
+++ app/components/Videos.js
@@ -1,6 +1,126 @@
+//nicely designed and implemented version of the code
 var React=require('react');
 
-var Videos= React.createClass({
+/*
+//TODO move to external file
+//import videosJSON from ('videos.json');
+*/
+
+var videosJSON = {"videos": [
+        {
+            "title": "Starting the compressor",
+            "src": "starting_the_compressor.mp4",
+            "subsection": false,
+            "section": ""
+        },
+        {
+            "title": "Drum Selection",
+            "src": "drum_selection.mp4",
+            "subsection": false,
+            "section": ""
+        },
+        {
+            "title": "Drum Pickup",
+            "src": "drum pick-up.mp4",
+            "subsection": false,
+            "section": ""
+        },
+        {
+            "title": "Removing a Drum from the Grease Stand",
+            "src": "operation_of_grease_stand_removing_drum.mp4",
+            "subsection": true,
+            "section": "Operation of Grease Stand"
+        },
+        {
+            "title": "Removing a Dented Drum from the Grease Stand",
+            "src": "operation_of_grease_stand_removing_dented_drum.mp4",
+            "subsection": true,
+            "section": "Operation of Grease Stand"
+        },
+        {
+            "title": "Operating the Grease Stand",
+            "src": "operation_of_grease_stand.mp4",
+            "subsection": true,
+            "section": "Operation of Grease Stand"
+        },
+        {
+            "title": "Dealing With a Water Contaminated Drum",
+            "src": "opereation_of_grease_stand_water_contamination.mp4",
+            "subsection": true,
+            "section": "Operation of Grease Stand"
+        },
+        {
+            "title": "Heavy Metals",
+            "src": "heavy_metals.mp4",
+            "subsection": false,
+            "section": ""
+        },
+        {
+            "title": "Starting The Mixing Machine",
+            "src": "mixing_machine_starting.mp4",
+            "subsection": true,
+            "section": "The Mixing Machine"
+        },
+        {
+            "title": "Adding Additives to the Mixing Machine (Heavy Metals, Tackifier, Setal)",
+            "src": "mixing_machine_additives.mp4",
+            "subsection": true,
+            "section": "The Mixing Machine"
+        },
+        {
+            "title": "Extractor Operation",
+            "src": "extractor_operation.mp4",
+            "subsection": false,
+            "section": ""
+        },
+        {
+            "title": "Floveyor Operation",
+            "src": "floveyor_operation.mp4",
+            "subsection": false,
+            "section": ""
+        },
+        {
+            "title": "Rinstrum R420 Operation (Premium Lube)",
+            "src": "load_cell_operation_r420.mp4",
+            "subsection": true,
+            "section": "Load Cell Operation"
+        },
+        {
+            "title": "Rinstrum R2100 Operation (ZN50)",
+            "src": "load_cell_operation_r2100.mp4",
+            "subsection": true,
+            "section": "Load Cell Operation"
+        },
+        {
+            "title": "Cleaning the Filter",
+            "src": "load_cell_operation_clearning_filter.mp4",
+            "subsection": true,
+            "section": "Load Cell Operation"
+        },
+        {
+            "title": "Packaging Selection",
+            "src": "packaging_selection.mp4",
+            "subsection": false,
+            "section": ""
+        },
+        {
+            "title": "Wrapping of a Pallet",
+            "src": "wrapping_a_pallet.mp4",
+            "subsection": false,
+            "section": ""
+        },
+        {
+            "title": "ZN50",
+            "src": "ZN50.mp4",
+            "subsection": false,
+            "section": ""
+        }
+    ]
+};
+var videosSectionArray = new Array();
+var videosData = new Array();
+
+var Videos=React.createClass({
     propTypes: {
         title:          React.PropTypes.string,
         src:            React.PropTypes.string,
@@ -8,20 +128,82 @@ var Videos= React.createClass({
         section:        React.PropTypes.string
     },
 
-    render: function () {
-        var title=this.props.title;
-        var src=this.props.src;
-        var subSection=this.props.subSection;
-        var section=this.props.section;
+    createVideoSections: function(videoSection){
+        //build an array of the various sections
+        if(videosSectionArray.indexOf(videoSection)===-1) {
+            videosSectionArray.push(videoSection);
+        }
+        //todo add the array to state
+    },
+
+    createVideoContentsArrays: function(video){
+        this.createVideoSections(video.section);//add all sections to an array
+
+        var currentVideoData=[
+            [video.title,video.src,video.subsection],
+        ];
+
+        var currentSectionIndex=videosSectionArray.indexOf(video.section);
+        var currentVideosDataArrayRowContents=videosData[currentSectionIndex];//contents of the row at the moment
+
+        if(currentVideosDataArrayRowContents!=undefined){
+            currentVideosDataArrayRowContents.push(currentVideoData);
+            videosData[currentSectionIndex]=currentVideosDataArrayRowContents; //add this video data to the array row that already exisits
+        }
+        else{
+            videosData.push(currentVideoData); //add this video data to an array as a new row
+        }
+
+    },
+
+    createVideos: function (video) {
+        return (
+            <div key={video.title}>
+                <a href={video.src}>
+                    {video.title}, {video.src}, {video.subSection}, {video.section}
+                </a>
+            </div>
+        )
+    },
+
+    createHeading: function(sectionIndex) {
+        var sectionHeading=videosSectionArray[sectionIndex];
+
+        return (
+            <h3>{sectionHeading}</h3>
+        )
+    },
+
+    createLists: function(listData){
+        listData.map(this.createVideoContentsArrays);
+
+        var videoSectionsData = videosSectionArray.map(function(headingName, index){
+
+            var sectionData =  videosData[index].map(function(dataElements, index){
+                return <li>{dataElements}</li>;
+            });
+
+            return (
+                <div>
+                    <h3>{headingName}</h3>
+                    <ul>{sectionData}</ul>
+                </div>
+            )
+        });
 
         return (
             <div>
-                <ul>
-                    <li>{title}</li>
-                    <li>{src}</li>
-                    <li>{subSection}</li>
-                    <li>{section}</li>
-                </ul>
+                {videoSectionsData}
+            </div>
+        );
+    },
+
+    render: function () {
+        var dataToRender =  this.createLists(videosJSON.videos );
+
+        return (
+            <div>
+                {dataToRender}
             </div>
         );
     }
--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var App = require('./components/VideosContainer');
+//var App = require('./components/VideosContainer');
+var App = require('./components/Videos');
 
 ReactDOM.render(
     <App />,
